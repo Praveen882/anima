e8b80a8e9774961db0172e4f796c1a3704b860a9
DhineshGitHub <dineshmurugesan7373@gmail.com>
2021-12-12 21:14:56 +0530

    signUp Page

--- src/signup.php
+++ src/signup.php
@@ -1,126 +1,155 @@
+<?php
+
+session_start();
+
+if (isset($_POST['signup'])) {
+  global $db;
+  require('db.php');
+
+  $New_Password = $_POST['User_New_Password'];
+  $Confirm_Password = $_POST['User_Confirm_Password'];
+  $password;
+
+  if ($New_Password == $Confirm_Password) {
+    $password = $New_Password;
+  } else {
+    $password = null;
+    echo "<script>alert('Password not match')</script>";
+  }
+
+  $username = $_POST['User_Name'];
+  $email = $_POST['User_Email'];
+  $query = "INSERT INTO login(UserName,UserEmail,UserPassword) VALUES('$username','$email','$password')";
+
+  if ($db-> query($query)) {
+    echo '<script>alert("SignUp Successfully")</script>';
+  } else {
+    echo '<script>alert("Something Wrong")</script>';
+  }
+}
+
+?>
+
+
 <!DOCTYPE html>
 <html>
-  <style>
-    /*set border to the form*/
+<style>
+  /*set border to the form*/
 
-    form {
-      border: 3px solid lightcoral;
-      margin-right: 30%;
-      margin-left: 30%;
-      margin-top: 5%;
-    }
-    /*assign full width inputs*/
+  form {
+    border: 3px solid lightcoral;
+    margin-right: 30%;
+    margin-left: 30%;
+    margin-top: 5%;
+  }
 
-    h2 {
-      color: orange;
-      font-style: italic;
-    }
-    h1 {
-      color: lightseagreen;
-    }
-    input[type="text"],
-    input[type="password"] {
-      width: 100%;
-      padding: 8px 10px;
-      margin: 8px 0;
-      display: inline-block;
-      border: 1px solid lightcoral;
-      box-sizing: border-box;
-    }
-    /*set a style for the buttons*/
+  /*assign full width inputs*/
 
-    button {
-      background-color: lightseagreen;
-      color: white;
-      padding: 14px 20px;
-      margin: 8px 0;
-      border: none;
-      cursor: pointer;
-      width: 100%;
-    }
-    /* set a hover effect for the button*/
+  h2 {
+    color: orange;
+    font-style: italic;
+  }
 
-    button:hover {
-      opacity: 0.8;
-    }
-    /*set extra style for the cancel button*/
+  h1 {
+    color: lightseagreen;
+  }
 
-    .cancelbtn {
-      width: auto;
-      padding: 10px 18px;
-      background-color: #9c59e9;
-    }
-    /*centre the display image inside the container*/
+  input[type="text"],
+  input[type="password"] {
+    width: 100%;
+    padding: 8px 10px;
+    margin: 8px 0;
+    display: inline-block;
+    border: 1px solid lightcoral;
+    box-sizing: border-box;
+  }
 
-    .imgcontainer {
-      text-align: center;
-      margin: 24px 0 12px 0;
-    }
-    /*set image properties*/
+  /*set a style for the buttons*/
 
-    img.avatar {
-      width: 40%;
-      border-radius: 50%;
-    }
-    /*set padding to the container*/
+  button {
+    background-color: lightseagreen;
+    color: white;
+    padding: 14px 20px;
+    margin: 8px 0;
+    border: none;
+    cursor: pointer;
+    width: 100%;
+  }
 
-    .container {
-      padding: 16px;
-    }
-    /*set the forgot password text*/
+  /* set a hover effect for the button*/
 
-    span.psw {
-      float: right;
-      padding-top: 16px;
-    }
-    /*set styles for span and cancel button on small screens*/
+  button:hover {
+    opacity: 0.8;
+  }
 
-    @media screen and (max-width: 300px) {
-    }
-  </style>
+  /*set extra style for the cancel button*/
 
-  <body>
-    <center>
-      <h2>Create Account</h2>
-    </center>
-    <!--Step 1 : Adding HTML-->
-    <form action="/action_page.php">
-      <div class="imgcontainer">
-        <img src="./img/login.gif" alt="Avatar" class="avatar" />
-        <h1>ANIMA</h1>
-      </div>
+  .cancelbtn {
+    width: auto;
+    padding: 10px 18px;
+    background-color: #9c59e9;
+  }
 
-      <div class="container" style="color: crimson">
-        <label><b>Name</b></label>
-        <input
-          type="text"
-          placeholder="Enter Your Name"
-          name="uname"
-          required
-        />
+  /*centre the display image inside the container*/
 
-        <label><b>Gmail</b></label>
-        <input type="text" placeholder="Enter gmail" name="uname" required />
+  .imgcontainer {
+    text-align: center;
+    margin: 24px 0 12px 0;
+  }
 
-        <label><b>Password</b></label>
-        <input
-          type="password"
-          placeholder="Enter Password"
-          name="psw"
-          required
-        />
+  /*set image properties*/
 
-        <label><b>Confirm Password</b></label>
-        <input
-          type="password"
-          placeholder="Enter Confirm Password"
-          name="psw"
-          required
-        />
+  img.avatar {
+    width: 40%;
+    border-radius: 50%;
+  }
 
-        <button type="submit">Create Account</button>
-        <input type="checkbox" checked="true" /> Remember me
-      </div>
-    </form>
-  </body>
+  /*set padding to the container*/
+
+  .container {
+    padding: 16px;
+  }
+
+  /*set the forgot password text*/
+
+  span.psw {
+    float: right;
+    padding-top: 16px;
+  }
+
+  /*set styles for span and cancel button on small screens*/
+
+  @media screen and (max-width: 300px) {}
+</style>
+
+<body>
+  <center>
+    <h2>Create Account</h2>
+  </center>
+  <!--Step 1 : Adding HTML-->
+  <form action="" method="post">
+    <div class="imgcontainer">
+      <img src="./img/login.gif" alt="Avatar" class="avatar" />
+      <h1>ANIMA</h1>
+    </div>
+
+    <div class="container" style="color: crimson">
+      <label><b>Name</b></label>
+      <input type="text" placeholder="Enter Your Name" name="User_Name" required />
+
+      <label><b>Gmail</b></label>
+      <input type="text" placeholder="Enter gmail" name="User_Email" required />
+
+      <label><b>Password</b></label>
+      <input type="password" placeholder="Enter Password" name="User_New_Password" required />
+
+      <label><b>Confirm Password</b></label>
+      <input type="password" placeholder="Enter Confirm Password" name="User_Confirm_Password" required />
+
+      <button type="submit" name="signup">Create Account</button>
+      <input type="checkbox" checked="true" /> Remember me
+    </div>
+  </form>
+</body>
+
 </html>
